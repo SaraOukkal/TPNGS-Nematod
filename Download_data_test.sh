3d17734e9b71538187a5689b99d54be208fb37e7
Oukkal Sara <sara.oukkal@ens-lyon.fr>
2020-11-17 10:14:06 +0000

    JOUR2: modification du script pour télécharger les données test (ajout du compteur de temps)

--- Download_data_test.sh
+++ Download_data_test.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
-##Download SRA Data (5 strains/3 replicates):
 
-### Create a directory:
+#Download SRA Data (5 strains/3 replicates):
+
+## Create a directory:
 data="/home/rstudio/data/mydatalocal/data"
 mkdir -p $data
 cd $data
 mkdir -p sra_data_test
 cd sra_data_test
+mkdir -p time_files
 
-### Create a list of SRR accessions:
+## Create a list of SRR accessions:
 SRR="SRR5564855
 SRR5564856
 SRR5564857
@@ -19,8 +21,12 @@ SRR5564867
 SRR5564868
 SRR5564869"
 
-###For loop to download data from each SRR: 
+##For loop to download data from each SRR: 
 for i in $SRR
 do 
+start=$SECONDS
 fastq-dump --split-3 --gzip -X 100000 --defline-seq '@$ac_$si/$ri' --defline-qual "+" $i 
+end=$SECONDS
+Time=$(((end - start)/60))
+echo "$i $Time" >> time_files/time_download_test.txt
 done
