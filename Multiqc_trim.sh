5c2113284c49e6ae6c3b5ace04602839f7b30f7a
SaraOukkal <sara.oukkal@ens-lyon.fr>
2020-12-14 10:11:56 +0000

    Script multiQC et README

---
+++ Multiqc_trim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Regroup quality controls (multiqc): 
+
+##Create a variable with the data set and the time files directory: 
+data="/home/rstudio/data/mydatalocal/data/fastqc_data_trim"
+time_files="/home/rstudio/data/mydatalocal/data/time_files"
+
+## Create a folder for the multiqc
+multiqc_data="/home/rstudio/data/mydatalocal/data/multiqc_data_trim"
+mkdir -p $multiqc_data
+cd $multiqc_data
+
+##Loop to run fastqc:
+start=$SECONDS
+multiqc $data/* -o . 
+end=$SECONDS
+Time=$(((end - start)/60))
+echo "$file $Time" >> $time_files/time_multiqc_trim.txt
